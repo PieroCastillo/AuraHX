851df84f40da6c3671c93c4d7e4589fd9cb552c1
PieroCastillo <39932324+PieroCastillo@users.noreply.github.com>
2020-04-12 14:45:15 -0500

    experimental

---
+++ src/Aura.Maths/2D/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aura.Maths
+{
+    public class Rectangle : Poligon, ISizeable
+    {
+        public int Height{get;set;}
+        public int Width{get;set;}
+    }
+}
---
+++ src/Aura.Maths/ISizeable.cs
@@ -0,0 +1,8 @@
+namespace Aura.Maths
+{
+    public interface ISizeable
+    {
+         int Height{get; set;}
+         int Width{get; set;}
+    }
+}
